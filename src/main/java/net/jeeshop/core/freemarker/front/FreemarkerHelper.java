31e7f7d76710d05de378f070827b4f40c95b16b2
jack13163 <18163132129@163.com>
2019-10-09 14:40:58 +0800

    modefy

--- src/main/java/net/jeeshop/core/freemarker/front/FreemarkerHelper.java
+++ src/main/java/net/jeeshop/core/freemarker/front/FreemarkerHelper.java
@@ -1,5 +1,26 @@
 package net.jeeshop.core.freemarker.front;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import javax.servlet.ServletContext;
+
+import org.apache.commons.lang.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.ContextLoader;
+import org.springframework.web.context.WebApplicationContext;
+
 import freemarker.template.Configuration;
 import freemarker.template.Template;
 import freemarker.template.TemplateException;
@@ -8,18 +29,6 @@ import net.jeeshop.services.front.news.bean.News;
 import net.jeeshop.services.front.product.ProductService;
 import net.jeeshop.services.front.product.bean.Product;
 import net.jeeshop.web.util.RequestHolder;
-import org.apache.commons.lang.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import javax.servlet.ServletContext;
-import java.io.*;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Locale;
-import java.util.Map;
 
 /**
  * 生成模板的帮助类
@@ -34,12 +43,12 @@ public class FreemarkerHelper {
 	private ProductService productService;
 	@Autowired
 	private NewsService newsService;
-	
+
 	/**
 	 * 模板
 	 */
-	public static final String template_newsInfo = "newsInfo.ftl";//文章模板
-	public static final String template_product = "product.ftl";//商品介绍模板
+	public static final String template_newsInfo = "newsInfo.ftl";// 文章模板
+	public static final String template_product = "product.ftl";// 商品介绍模板
 
 	public void setProductService(ProductService productService) {
 		this.productService = productService;
@@ -54,50 +63,47 @@ public class FreemarkerHelper {
 	 * 
 	 * @return
 	 */
-//	public void index(String template, String templateHtml) {
-//		try {
-//			// 准备数据
-//			HashMap<String, Object> data = new HashMap<String, Object>();
-//			data.put("indexMenuList", loadIndexMenu());
-//			crateHTML(ServletActionContext.getServletContext(), data, template,
-//					templateHtml);
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-//	}
+	public void index(String template, String templateHtml) {
+		try {
+			// 准备数据
+			HashMap<String, Object> data = new HashMap<String, Object>();
+			data.put("indexMenuList", loadIndexMenu());
+
+			// 获取servletcontext
+			WebApplicationContext webApplicationContext = ContextLoader.getCurrentWebApplicationContext();
+			ServletContext servletContext = webApplicationContext.getServletContext();
+
+			crateHTML(servletContext, data, template, templateHtml);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * 加载门户菜单列表
 	 * 
 	 * @return
-	 * @return
 	 */
-//	private List<IndexMenu> loadIndexMenu() {
-//		IndexMenu e = new IndexMenu();
-//		List<IndexMenu> indexMenuList = indexMenuService.selectList(e);
-//		return indexMenuList;
-//	}
+	private String loadIndexMenu() {
+		return "";
+	}
 
 	/**
 	 * 生成静态页面主方法
 	 * 
-	 * @param context
-	 *            ServletContext
-	 * @param data
-	 *            一个Map的数据结果集
-	 * @param templatePath
-	 *            ftl模版路径
-	 * @param targetHtmlPath
-	 *            生成静态页面的路径
-	 * @throws IOException 
-	 * @throws TemplateException 
+	 * @param context        ServletContext
+	 * @param data           一个Map的数据结果集
+	 * @param templatePath   ftl模版路径
+	 * @param targetHtmlPath 生成静态页面的路径
+	 * @throws IOException
+	 * @throws TemplateException
 	 */
-	public void crateHTML(ServletContext context, Map<String, Object> data,
-			String templatePath, String targetHtmlPath) throws Exception {
-		if(StringUtils.isBlank(targetHtmlPath)){
+	public void crateHTML(ServletContext context, Map<String, Object> data, String templatePath, String targetHtmlPath)
+			throws Exception {
+		if (StringUtils.isBlank(targetHtmlPath)) {
 			throw new NullPointerException("targetHtmlPath不能为空！");
 		}
-		
+
 		Configuration freemarkerCfg = new Configuration();
 		// 加载模版
 		freemarkerCfg.setServletContextForTemplateLoading(context, "/template");
@@ -105,26 +111,21 @@ public class FreemarkerHelper {
 		freemarkerCfg.setDefaultEncoding("UTF-8");
 
 		// 指定模版路径
-		Template template = freemarkerCfg
-				.getTemplate(templatePath, "UTF-8");
+		Template template = freemarkerCfg.getTemplate(templatePath, "UTF-8");
 		template.setEncoding("UTF-8");
-		// 静态页面路径
-//			String htmlPath = context.getRealPath("/") + "/"
-//					+ targetHtmlPath;
 		System.out.println(targetHtmlPath);
 		File htmlFile = new File(targetHtmlPath);
-		Writer out = new BufferedWriter(new OutputStreamWriter(
-				new FileOutputStream(htmlFile), "UTF-8"));
+		Writer out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(htmlFile), "UTF-8"));
 		// 处理模版
 		template.process(data, out);
 		out.flush();
 		out.close();
 		System.out.println("生成成功");
-//		try {
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//			System.out.println("生成失败");
-//		}
+		try {
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println("生成失败");
+		}
 	}
 
 	public boolean isEmpty(String value) {
@@ -133,101 +134,113 @@ public class FreemarkerHelper {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * 对系统帮助的文章静态化
-	 * @throws Exception 
+	 * 
+	 * @throws Exception
 	 */
 	public void helps() throws Exception {
 		News param = new News();
 		param.setType("help");
 		List<News> notices = newsService.selectList(param);
-		if(notices==null || notices.size()==0){
+		if (notices == null || notices.size() == 0) {
 			logger.error("notices size = 0");
 			return;
 		}
 		logger.error("notices size = " + notices.size());
-		
+
+		String htmlDirPath = RequestHolder.getSession().getServletContext().getRealPath("/") + "/html/helps/";
+		createDirsIfNotExist(htmlDirPath);
+
 		HashMap<String, Object> data = new HashMap<String, Object>();
-		for(int i=0;i<notices.size();i++){
+		for (int i = 0; i < notices.size(); i++) {
 			News news = notices.get(i);
-			if(StringUtils.isBlank(news.getContent())){
+			if (StringUtils.isBlank(news.getContent())) {
 				continue;
 			}
-			
+
 			data.clear();
 			data.put("e", news);
-			String templateHtml = RequestHolder.getSession().getServletContext().getRealPath("/")+"/jsp/helps/"+news.getId()+".jsp";
-			crateHTML(RequestHolder.getSession().getServletContext(), data, template_newsInfo,templateHtml);
-			logger.error("生成html页面成功！id="+news.getId());
+
+			String templateHtml = htmlDirPath + news.getId() + ".html";
+			crateHTML(RequestHolder.getSession().getServletContext(), data, template_newsInfo, templateHtml);
+			logger.error("生成html页面成功！id=" + news.getId());
 		}
 	}
-	
+
 	/**
 	 * 对新闻公告的文章静态化
-	 * @throws Exception 
+	 * 
+	 * @throws Exception
 	 */
 	public void notices() throws Exception {
 		News param = new News();
 		param.setType("notice");
 		List<News> notices = newsService.selectList(param);
-		if(notices==null || notices.size()==0){
+		if (notices == null || notices.size() == 0) {
 			logger.error("notices size = 0");
 			return;
 		}
 		logger.error("notices size = " + notices.size());
-		
+
+		String htmlDirPath = RequestHolder.getSession().getServletContext().getRealPath("/") + "html/notices/";
+		createDirsIfNotExist(htmlDirPath);
+
 		HashMap<String, Object> data = new HashMap<String, Object>();
-		for(int i=0;i<notices.size();i++){
+		for (int i = 0; i < notices.size(); i++) {
 			News news = notices.get(i);
-			if(StringUtils.isBlank(news.getContent())){
+			if (StringUtils.isBlank(news.getContent())) {
 				continue;
 			}
-			
+
 			data.clear();
 			data.put("e", news);
-			String templateHtml = RequestHolder.getSession().getServletContext().getRealPath("/")+"/jsp/notices/"+news.getId()+".jsp";
-			crateHTML(RequestHolder.getSession().getServletContext(), data, template_newsInfo,templateHtml);
-			logger.error("生成html页面成功！id="+news.getId());
+			String templateHtml = htmlDirPath + news.getId() + ".html";
+			crateHTML(RequestHolder.getSession().getServletContext(), data, template_newsInfo, templateHtml);
+			logger.error("生成html页面成功！id=" + news.getId());
 		}
 	}
 
 	/**
 	 * 对商品介绍静态化
-	 * @throws Exception 
+	 * 
+	 * @throws Exception
 	 */
 	public String products() throws Exception {
 		List<Product> productList = productService.selectListProductHTML(new Product());
-		if(productList==null || productList.size()==0){
+		if (productList == null || productList.size() == 0) {
 			logger.error("productList size = 0");
 			return null;
 		}
 		logger.error("productList size = " + productList.size());
 		StringBuilder errorBuff = new StringBuilder();
-		
+
+		String htmlDirPath = RequestHolder.getSession().getServletContext().getRealPath("/") + "html/product/";
+
 		HashMap<String, Object> data = new HashMap<String, Object>();
-		for(int i=0;i<productList.size();i++){
+		for (int i = 0; i < productList.size(); i++) {
 			Product p = productList.get(i);
-			if(StringUtils.isBlank(p.getProductHTML())){
+			if (StringUtils.isBlank(p.getProductHTML())) {
 				continue;
 			}
-			
+
 			data.clear();
 			data.put("e", p);
-			String templateHtml = RequestHolder.getSession().getServletContext().getRealPath("/")+"/jsp/product/"+p.getId()+".jsp";
+			String templateHtml = htmlDirPath + p.getId() + ".jsp";
 			try {
-				crateHTML(RequestHolder.getSession().getServletContext(), data, template_product,templateHtml);
+				crateHTML(RequestHolder.getSession().getServletContext(), data, template_product, templateHtml);
 			} catch (Exception e) {
 				e.printStackTrace();
-				logger.error("生成html页面失败！id="+p.getId());
-				
-				errorBuff.append(p.getId()+",");
+				logger.error("生成html页面失败！id=" + p.getId());
+
+				errorBuff.append(p.getId() + ",");
 				continue;
 			}
-			logger.error("生成html页面成功！id="+p.getId());
+			logger.error("生成html页面成功！id=" + p.getId());
 		}
-		
-		if(errorBuff.length()==0){
+
+		if (errorBuff.length() == 0) {
 			return null;
 		}
 		return errorBuff.toString();
@@ -235,57 +248,83 @@ public class FreemarkerHelper {
 
 	/**
 	 * 静态化指定的商品
+	 * 
 	 * @param id
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public String staticProductByID(String id) throws Exception {
 		Product p = productService.selectById(id);
-		if(p==null || StringUtils.isBlank(p.getProductHTML())){
+		if (p == null || StringUtils.isBlank(p.getProductHTML())) {
 			logger.error("ERROR,not found product by id = " + id);
 			throw new NullPointerException("ERROR,not found product by id = " + id);
 		}
-		
+
 		HashMap<String, Object> data = new HashMap<String, Object>();
 		data.clear();
 		data.put("e", p);
-		String templateHtml = RequestHolder.getSession().getServletContext().getRealPath("/")+"/jsp/product/"+p.getId()+".jsp";
-		crateHTML(RequestHolder.getSession().getServletContext(), data, template_product,templateHtml);
-		logger.error("生成html页面成功！id="+p.getId());
-		
+
+		String htmlDirPath = RequestHolder.getSession().getServletContext().getRealPath("/") + "html/product/";
+		createDirsIfNotExist(htmlDirPath);
+
+		String templateHtml = htmlDirPath + p.getId() + ".html";
+		crateHTML(RequestHolder.getSession().getServletContext(), data, template_product, templateHtml);
+		logger.error("生成html页面成功！id=" + p.getId());
+
 		return "success";
 	}
-	
+
 	/**
 	 * 静态化指定的文章
+	 * 
 	 * @param id
 	 * @return
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public String staticNewsByID(String id) throws Exception {
-		if(StringUtils.isBlank(id)){
+		if (StringUtils.isBlank(id)) {
 			throw new NullPointerException("id参数不能为空！");
 		}
-		
+
 		News news = newsService.selectById(id);
-		if(news==null || StringUtils.isBlank(news.getContent())){
+		if (news == null || StringUtils.isBlank(news.getContent())) {
 			logger.error("ERROR,not found news by id = " + id);
 			throw new NullPointerException("ERROR,not found news by id = " + id);
 		}
-		
+
 		HashMap<String, Object> data = new HashMap<String, Object>();
 		data.clear();
 		data.put("e", news);
 		String templateHtml = null;
-		
-		if(news.getType().equals(News.news_type_help)){
-			templateHtml = RequestHolder.getSession().getServletContext().getRealPath("/")+"/jsp/helps/"+news.getId()+".jsp";
-		}else if(news.getType().equals(News.news_type_notice)){
-			templateHtml = RequestHolder.getSession().getServletContext().getRealPath("/")+"/jsp/notices/"+news.getId()+".jsp";
+
+		if (news.getType().equals(News.news_type_help)) {
+			String htmlDirPath = RequestHolder.getSession().getServletContext().getRealPath("/") + "/html/helps/";
+			createDirsIfNotExist(htmlDirPath);
+			templateHtml = htmlDirPath + news.getId() + ".html";
+		} else if (news.getType().equals(News.news_type_notice)) {
+			String htmlDirPath = RequestHolder.getSession().getServletContext().getRealPath("/") + "/html/notice/";
+			createDirsIfNotExist(htmlDirPath);
+			templateHtml = htmlDirPath + news.getId() + ".html";
 		}
-		crateHTML(RequestHolder.getSession().getServletContext(), data, template_newsInfo,templateHtml);
-		logger.error("生成html页面成功！id="+news.getId());
-		
+		crateHTML(RequestHolder.getSession().getServletContext(), data, template_newsInfo, templateHtml);
+		logger.error("生成html页面成功！id=" + news.getId());
+
 		return "success";
+	}
+
+	/**
+	 * 判断目录是否存在，若不存在则新建
+	 * 
+	 * @param path
+	 */
+	private void createDirsIfNotExist(String path) {
+		File dir = new File(path);
+		if (!dir.exists()) {
+			if (dir.mkdirs()) {
+				System.out.println("创建静态页文件夹: " + dir);
+			} else {
+				System.out.println("创建静态页文件夹 " + dir + " 失败");
+			}
+		}
 	}
 }
--- src/main/java/net/jeeshop/web/action/manage/freemarker/FreemarkerAction.java
+++ src/main/java/net/jeeshop/web/action/manage/freemarker/FreemarkerAction.java
@@ -4,8 +4,6 @@
  */
 package net.jeeshop.web.action.manage.freemarker;
 
-import net.jeeshop.core.freemarker.front.FreemarkerHelper;
-import net.jeeshop.web.util.RequestHolder;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -14,9 +12,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import net.jeeshop.core.freemarker.front.FreemarkerHelper;
+import net.jeeshop.web.util.RequestHolder;
 
 /**
  * 网站会员action
+ * 
+ * 只有后台才可以静态化页面
  * 
  * @author li
  * 
@@ -25,7 +27,6 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("freemarker")
 public class FreemarkerAction {
 	private static final Logger logger = LoggerFactory.getLogger(FreemarkerAction.class);
-	private static final long serialVersionUID = 1L;
 	@Autowired
 	private FreemarkerHelper freemarkerHelper;
 
@@ -43,80 +44,42 @@ public class FreemarkerAction {
 	}
 
 	/**
-	 * 生成门户菜单
-	 * 
-	 * @return
-	 * @throws java.io.IOException
-	 */
-//	@Deprecated
-//	public String indexMenu() {
-//		freemarkerHelper.index("indexMenu.ftl", "indexMenu.jsp");
-//		return SUCCESS;
-//	}
-	
-	/**
 	 * 对商品的详情内容、帮助文章的内容、公告、新闻等的内容进行静态化处理。
 	 * 方法是将这些大文本的内容存储未jsp的文件格式，将来访问页面的时候直接把地址链接进去就行了。
+	 * 
 	 * @return
 	 * @throws Exception
 	 */
 	@RequestMapping("create")
 	@ResponseBody
-	public String create() throws Exception{
+	public String create() throws Exception {
 		String method = RequestHolder.getRequest().getParameter("method");
 		logger.error("create method = " + method);
-		if(StringUtils.isBlank(method)){
-			
-		}else if(method.equals("helps")){
-			freemarkerHelper.helps();//所有帮助文件静态化
-		}else if(method.equals("notices")){
-			freemarkerHelper.notices();//所有公告通知静态化
-		}else if(method.equals("products")){
-			String error = freemarkerHelper.products();//所有商品描述静态化
-			if(error==null){
+		if (StringUtils.isBlank(method)) {
+
+		} else if (method.equals("helps")) {
+			freemarkerHelper.helps();// 所有帮助文件静态化
+		} else if (method.equals("notices")) {
+			freemarkerHelper.notices();// 所有公告通知静态化
+		} else if (method.equals("products")) {
+			String error = freemarkerHelper.products();// 所有商品描述静态化
+			if (error == null) {
 				return ("success");
-			}else{
-				return ("部分商品静态化失败，商品ID："+error);
+			} else {
+				return ("部分商品静态化失败，商品ID：" + error);
 			}
-		}else if(method.equals("staticProductByID")){
+		} else if (method.equals("staticProductByID")) {
 			String id = RequestHolder.getRequest().getParameter("id");
-			
-			String response = freemarkerHelper.staticProductByID(id);//所有商品描述静态化
+
+			String response = freemarkerHelper.staticProductByID(id);// 所有商品描述静态化
 			return (response);
-		}else if(method.equals("staticNewsByID")){
+		} else if (method.equals("staticNewsByID")) {
 			String id = RequestHolder.getRequest().getParameter("id");
-			
-			String response = freemarkerHelper.staticNewsByID(id);//所有商品描述静态化
+
+			String response = freemarkerHelper.staticNewsByID(id);// 所有商品描述静态化
 			return (response);
 		}
-		
-		return ("success");
-//		return SUCCESS;
-	}
-	
-//	public String helps(){
-//		String method = getRequest().getParameter("method");
-//		if(StringUtils.isBlank(method)){
-//			
-//		}else if(method.equals("staticNews")){
-//			//生成所有的文章静态页面
-//			freemarkerHelper.staticNews();
-//		}else if(method.equals("staticNews")){
-//			//对单个文章静态化
-//			String id = getRequest().getParameter("id");
-//			freemarkerHelper.staticNewsInfo(id);
-//		}
-//		return SUCCESS;
-//	}
 
-	/**
-	 * 一键生成
-	 * 
-	 * @return
-	 */
-//	@Deprecated
-//	public String createAll() {
-//		indexMenu();
-//		return SUCCESS;
-//	}
+		return ("success");
+	}
 }
